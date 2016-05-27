fe45972100b5888c188e844c29c86256cdc00274
lzy <you@example.com>
2016-05-27 18:10:09 +0800

    Initial Commit

---
+++ BabySong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BabySong
+//
+//  Created by 李 战阳 on 16/5/27.
+//  Copyright © 2016年 findsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BabySong/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BabySong
+//
+//  Created by 李 战阳 on 16/5/27.
+//  Copyright © 2016年 findsoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
